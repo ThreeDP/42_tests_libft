2a69fd360190d022ad787ac090552149a8c989a2
threedp.png <davypaulinocbjr@outlook.com>
2022-09-08 21:11:38 +0200

    Implementação do header de test

---
+++ test.h
@@ -0,0 +1,11 @@
+#ifndef __TEST_H__
+# define __TEST_H__
+#include <stdio.h>
+#include <ctype.h>
+#include "../libft.h"
+
+#define PRINTRED(x)  "\x1B[31m"x"\x1B[37m"
+#define PRINTGRN(x)  "\x1B[32m"x"\x1B[37m"
+#define PRINTYEL(x)  "\x1B[33m"x"\x1B[37m"
+
+#endif
