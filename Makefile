554cbe3ecb560c27f66efb699d608af105f82cbc
threedp.png <davypaulinocbjr@outlook.com>
2022-09-16 17:11:49 +0200

    Implementação do teste para a função isalnum

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 PROGS = ft_isalpha \
-		ft_isdigit
+		ft_isdigit \
+		ft_isalnum
+#		ft_memcpy
 SRCS =	ft_isalpha_test.c \
-		ft_isdigit_test.c
+		ft_isdigit_test.c \
+		ft_isalnum_test.c
+#		ft_memcpy_test.c
+OBJS := $(SRCS:.c=.o)
 DIR = ../
 LIB = libft.a
 HEADERS = test.h
@@ -9,7 +14,7 @@ FLAGS = -Wall -Werror -Wextra
 
 all: $(SRCS:.c=.o) $(PROGS)
 
-$(PROGS): $(addprefix $(DIR), $(LIB))
+$(PROGS): $(addprefix $(DIR), $(LIB)) $(OBJS)
 	gcc $(FLAGS) $@_test.o -I $(DIR) -L $(DIR) -lft -o $@
 	@echo
 	@echo INICIANDO TESTE DE...
@@ -27,12 +32,11 @@ norm:
 	@cd $(DIR) && $(MAKE) $@
 
 clean:
-	rm -f $(SRCS:.c=.o)
+	@rm -f $(SRCS:.c=.o)
 	@cd $(DIR) && $(MAKE) $@
 
 fclean:
-	rm -f $(PROGS)
+	@rm -f $(PROGS)
 	@cd $(DIR) && $(MAKE) $@
-
 
 re: fclean all
---
+++ ft_isalnum_test.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum_test.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 19:19:50 by coder             #+#    #+#             */
+/*   Updated: 2022/09/16 17:05:17 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+void	imprimir(char c, int *line)
+{
+	int char_int;
+	int result_my_f;
+	int result_lib;
+
+	char_int = c;
+	result_lib = isalnum(char_int);
+	result_my_f = ft_isalnum(char_int);
+	printf("\nA letra "PRINTYEL("'%c'")" é um alfanumérico?\n", c);
+	printf("Valor esperado é de: "PRINTYEL("%d")"\n", result_lib);
+	printf("Valor Recebido: "PRINTYEL("%d")"\n", result_my_f);
+	if (result_lib == result_my_f)
+		printf(PRINTGRN("SUCESSO!!"));
+	else
+	{
+		if (result_lib > 0 || result_lib > 0)
+			printf(PRINTGRN("SUCESSO!!"));
+		else
+			printf(PRINTRED("xxxERROxxx"));
+	}
+	printf("\n*=========================* Teste: %d", *line);
+	*line += 1;
+}
+
+void	testar()
+{
+	int line;
+
+	line = 1;
+	imprimir('A', &line);
+	imprimir('s', &line);
+	imprimir('0', &line);
+	imprimir('t', &line);
+	imprimir('V', &line);
+	imprimir('X', &line);
+	imprimir('*', &line);
+	imprimir('\t', &line);
+	imprimir('%', &line);
+	imprimir('z', &line);
+	imprimir('a', &line);
+	imprimir('x', &line);
+	imprimir('\n', &line);
+}
+
+int main()
+{
+	printf(PRINTYEL("--- INICIANDO TESTES ---\n\n"));
+	testar();
+	printf(PRINTYEL("\n\n--- FINAL TESTES ---\n\n"));
+}
